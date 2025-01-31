e75e8b26ec0284112b3a867abf616e8320294d4e
Luigi-Teodoro <luigi.tc14@gmail.com>
2025-01-30 19:10:21 -0500

    actualizacion

--- src/router/index.js
+++ src/router/index.js
@@ -4,5 +4,7 @@ const router = Router();
 
 router.get('/', (req, res) => res.render('index',{title:"Portafolio" })); 
 router.get('/sobremi', (req, res) => res.render('sobre_mi',{title:"Sobre Mi" })); 
+router.get('/habilidades', (req, res) => res.render('habilidades',{title:"Habilidades" })); 
+
 
 export default router;
